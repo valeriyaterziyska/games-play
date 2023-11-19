b2434446ed6c3d68fa02a05c7a39e6baf815d2fc
Valeria Terziyska <valeria.terziyska@gmail.com>
2023-11-19 18:03:21 +0200

    services, fetch

--- games-play/src/App.js
+++ games-play/src/App.js
@@ -1,3 +1,8 @@
+import { useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+import * as gameService from './services/gameService';
+
 import { Catalog } from "./components/Catalog/Catalog";
 import { CreateGame } from "./components/CreateGame/CreateGame";
 import { Footer } from "./components/Footer/Footer";
@@ -5,20 +10,45 @@ import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
+import { GameDetails } from './components/GameDetails/GameDetails';
 
 function App() {
+    const navigate = useNavigate();
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => {
+                console.log(result);
+                setGames(result);
+            })
+    }, []);
+
+    const onCreateGameSubmit = async (data) => {
+        const newGame = await gameService.create(data);
+
+        //TODO: add to state
+        setGames(state => [...state, newGame]);
+
+        //TODO: redirect to Catalog
+        navigate('/catalog');
+    };
+
     return (
         <div id="box">
             <Header />
 
             <main id="main-content">
-
+                <Routes>
+                    <Route path='/' element= {<Home />} />  
+                    <Route path='/login' element= {<Login />} />  
+                    <Route path='/register' element= {<Register />} />  
+                    <Route path='/create-game' element= {<CreateGame onCreateGameSubmit={onCreateGameSubmit}/>} />  
+                    <Route path='/catalog' element= {<Catalog games={games}/>} />
+                    <Route path='/catalog/:gameId' element={<GameDetails />} />  
+                </Routes>
             </main>
-            {/* <Home /> */}
-            {/* <Login /> */}
-            {/* <Register /> */}
-            {/* <CreateGame /> */}
-            <Catalog />
+        
             <Footer />
         </div>
     );
